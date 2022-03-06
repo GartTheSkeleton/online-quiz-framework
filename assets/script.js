a0837384f69f997e6a47e56d100d5244c9231444
Gart <gmhobson@gmail.com>
2022-03-06 15:20:22 -0600

    clicking start game now renders the first question title and 4 answer buttons

--- assets/script.js
+++ assets/script.js
@@ -15,13 +15,18 @@ let questionCounter = 0
 let highscores = [0,0,0,0,0];
 let highscoreNames = ["?","?","?","?","?"]
 
-const question1 = {question:"Question text here", answer1:"Answer text here", answer2:"Answer text here", answer3:"Answer text here", answer4:"Answer text here", correctAnswer:1}
+const question1 = {question:"Question text here", answer1:"Answer 1 text here", answer2:"Answer 2 text here", answer3:"Answer 3 text here", answer4:"Answer 4 text here", correctAnswer:1}
 const question2 = {question:"Question text here", answer1:"Answer text here", answer2:"Answer text here", answer3:"Answer text here", answer4:"Answer text here", correctAnswer:1}
 const question3 = {question:"Question text here", answer1:"Answer text here", answer2:"Answer text here", answer3:"Answer text here", answer4:"Answer text here", correctAnswer:1}
 const question4 = {question:"Question text here", answer1:"Answer text here", answer2:"Answer text here", answer3:"Answer text here", answer4:"Answer text here", correctAnswer:1}
 
 var questionArray = [question1, question2, question3, question4]
 
+var questionBtn1 = document.createElement("button")
+var questionBtn2 = document.createElement("button")
+var questionBtn3 = document.createElement("button")
+var questionBtn4 = document.createElement("button")
+var buttonArray = [questionBtn1, questionBtn2, questionBtn3, questionBtn4]
 
 //Function to begin the quiz from the title screen
 var quizStartQuiz = function(){
@@ -30,6 +35,14 @@ titleScreen.style.display = "none";
 var questionTitle = document.createElement("h1");
 questionTitle.textContent = questionArray[questionCounter].question;
 quizScreen.appendChild(questionTitle);
+buttonArray[0].textContent = questionArray[questionCounter].answer1;
+quizScreen.appendChild(buttonArray[0])
+buttonArray[1].textContent = questionArray[questionCounter].answer2;
+quizScreen.appendChild(buttonArray[1])
+buttonArray[2].textContent = questionArray[questionCounter].answer3;
+quizScreen.appendChild(buttonArray[2])
+buttonArray[3].textContent = questionArray[questionCounter].answer4;
+quizScreen.appendChild(buttonArray[3])
 }
 //Function to display the quiz's questions
 
